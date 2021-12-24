6b57f04fea00e509b4f84dac0ba6ce3714cede39
minami0601 <minami.moroishi.0601@gmail.com>
2021-12-24 17:47:11 +0900

    トップページの成形

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,27 @@
 <template>
-    <v-footer
-      :absolute="!fixed"
-      app
+  <v-footer
+    light
+  >
+    <v-row
+      justify="center"
+      no-gutters
     >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+      <v-col
+        class="py-4 text-center"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
 </template>
+
+<script>
+export default {
+  data() {
+    return{
+      links: ["ログイン","新規登録"]
+    }
+  },
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,38 +1,20 @@
 <template>
-  <v-app dark>
+  <v-app>
     <Header />
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <nuxt />
+    <Footer />
   </v-app>
 </template>
 
 <script>
-import Header from "@/components/Header";
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 
 export default {
   name: 'DefaultLayout',
   components: {
-    Header
+    Header,
+    Footer,
   },
   data () {
     return {
