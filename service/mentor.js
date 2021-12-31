e3f7545757666924fa32f4893372de1812d1ed1e
minami0601 <minami.love.bb@icloud.com>
2021-12-31 21:33:57 +0900

    メンターstore作成

--- service/mentor.js
+++ service/mentor.js
@@ -4,6 +4,6 @@ const backendURL = process.env.VUE_APP_BACKEND_URL;
 
 // おすすめメンター一覧取得
 export const getMentorRecommendAll = () => {
-  const response = axios.get(`${backendURL}/api/public/mentor_recommend`);
+  const response = axios.get('/api/public/mentor_recommend');
   return response;
 };
